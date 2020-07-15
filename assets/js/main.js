08e8398402a88813284d54926170ad1184dba738
dungbuids5 <dungdalatds5@gmail.com>
2020-07-16 01:08:07 +0700

    home4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,38 @@
 // $(function() {
-	
+
 // 	// Cache the Window object
 // 	var $window = $(window);
-	
+
 // 	// Parallax Backgrounds
 // 	// Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
-	
+
 // 	$('section[data-type="background"]').each(function(){
 // 		var $bgobj = $(this); // assigning the object
-		
+
 // 		$(window).scroll(function() {
-		
+
 // 			// Scroll the background at var speed
-// 			// the yPos is a negative value because we're scrolling it UP!								
+// 			// the yPos is a negative value because we're scrolling it UP!
 // 			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-			
+
 // 			// Put together our final background position
 // 			var coords = '50% '+ yPos + 'px';
-			
+
 // 			// Move the background
 // 			$bgobj.css({ backgroundPosition: coords });
-			
+
 // 		}); // end window scroll
 // 	});
-	
+
 // });
+const hamburger=document.querySelector(".hamburger");
+const navlink=document.querySelector(".nav-link-top");
+const links=document.querySelectorAll(".nav-link-top li");
+hamburger.addEventListener("click",()=>
+{
+navlink.classList.toggle("open");
+links.forEach(link => {
+link.classList.toggle("fade");
+});
+
+});
