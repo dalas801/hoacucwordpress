a36c46e2c4219d1cea6eb6b3dfc934e00915ae27
Dung Bui <dungdalatds@gmail.com>
2020-07-17 17:30:42 +0700

    home8

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,14 +25,38 @@
 // 	});
 
 // });
-const hamburger=document.querySelector(".hamburger");
-const navlink=document.querySelector(".nav-link-top");
-const links=document.querySelectorAll(".nav-link-top li");
-hamburger.addEventListener("click",()=>
-{
-navlink.classList.toggle("open");
-links.forEach(link => {
-link.classList.toggle("fade");
-});
+// const hamburger=document.querySelector(".hamburger");
+// const navlink=document.querySelector(".nav-link-top");
+// const links=document.querySelectorAll(".nav-link-top li");
+// hamburger.addEventListener("click",()=>
+// {
+// navlink.classList.toggle("open");
+// links.forEach(link => {
+// link.classList.toggle("fade");
+// });
 
-});
+// });
+
+// var burgerBtn = document.getElementById('burgerBtn');
+// var mobile = document.getElementById('mobile');
+
+
+// burgerBtn.addEventListener('click', function() {
+//     mobile.classList.toggle('navigation');
+//   }, false);
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    var site_header=document.querySelector(".body");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    site_header.classList.add("fixPosition");
+      document.querySelector(".logoImg").src="assets/img/logoscroll.png";
+    // document.getElementById("navbar").style.padding = "30px 10px";
+    // document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    
+    document.querySelector(".logoImg").src="assets/img/logo@2x.png";
+    site_header.classList.remove("fixPosition");
+    // document.getElementById("logo").style.fontSize = "35px";
+  }
+}
